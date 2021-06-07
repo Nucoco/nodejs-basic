f91c412947bb64a714920fd69a8019d249408f3e
Nucoco <yuuta11511997@gmail.com>
2021-06-08 08:30:03 +0900

    master

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+/**
+ * Beginner's Series to Node.js
+ * 1. Navigate filesystem
+ * 2. Find the files that have intended extension(like .json)
+ * 3. Join the paths with the chars corresponding to the OS
+ * 4. Create a new file or directory
+ * 5. Read and write a json file
+ */
+
 const fs = require("fs").promises;
 const path = require("path");
 
@@ -16,35 +25,37 @@ async function main() {
 
 	// Using __direname can get absolute path. By this, program outputs the same result regardless of where it's executed at.
 	// path.join() joins its params with correct separation character according to the OS.
-	console.log(await findSalesFiles(path.join(__dirname, "stores")));
 	const salesFiles = await findSalesFilesRecursively(path.join(__dirname, "stores"));
 	console.log(salesFiles);
+
+	const salesTotal = await calculateSalesTotal(salesFiles);
+	const report = {
+		salesTotal,
+		totalStores: salesFiles.length,
+	};
 
 	const reportPath = path.join(salesTotalsDir, "report.json");
 	// Remove the file since writeFile() opens a file with append mode by default. But what we gonna do is override.
 	try {
 		await fs.unlink(reportPath);
 	} catch {
-		console.log("Failed to remove");
+		console.log("Failed to remove" + reportPath);
 	}
 
-	// override
+	// Override
 	// filename, context
-	await fs.writeFile(path.join(salesTotalsDir, "report.json"), "");
+	// JSON.stringify: Converts a JavaScript value to a JavaScript Object Notation (JSON) string. Third param specifies the indent size.
+	// writeFile() takes string type.
+	await fs.writeFile(reportPath, JSON.stringify(report, null, 2));
 	console.log("Sales report written to " + salesTotalsDir);
 }
 
 main();
 
-async function findSalesFiles(folderName) {
-	// fs.readdir() takes foldername and returns the info inside it.
-	const storeFiles = await fs.readdir(folderName);
-	return storeFiles;
-}
-
 async function findSalesFilesRecursively(folderName) {
 	let salesFiles = [];
 
+	// fs.readdir() takes foldername and returns the info inside it.
 	// Taking the second param, it returns filenames with their meta info instead of only filenames
 	const items = await fs.readdir(folderName, { withFileTypes: true });
 
@@ -60,4 +71,16 @@ async function findSalesFilesRecursively(folderName) {
 		}
 	}
 	return salesFiles;
+}
+
+async function calculateSalesTotal(salesFiles) {
+	let salesTotal = 0;
+	for (file of salesFiles) {
+		// JSON.parse(): Converts a JavaScript Object Notation (JSON) string into an object. Also tolerates buffer.
+		// readFile() returns buffer not file contents. Buffer is a reserved area to store binary data in memory.
+		// If there is a necessity to read string data, give a encode format to the 2nd param.
+		const data = JSON.parse(await fs.readFile(file));
+		salesTotal += data.total;
+	}
+	return salesTotal;
 }
